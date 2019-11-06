6e6d62c32550aa216715fbdaa7c6c4dae8ad77d3
Nicolas Hervy <nicolas.hervy@spark-vision.com>
2019-11-06 11:48:24 +0100

    Add loop around collection.

--- snippets-array.js
+++ snippets-array.js
@@ -1,3 +1,14 @@
+/**
+ * Loop around a collection.
+ */
+[...document.querySelectorAll('div')].map(arrItem => {
+  console.log(item);
+});
+// ...or...
+[].forEach.call(document.querySelectorAll('div'), item => {
+  console.log(item);
+});
+
 /*
 1. all
 This snippet returns true if the predicate function returns true for all elements in a collection and false otherwise. You can omit the second argument fn if you want to use Boolean as a default value.
