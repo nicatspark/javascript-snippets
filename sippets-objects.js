20e92f4bb9ad94deea5038bd772b462f86680dc5
Nicolas Hervy <nicolas.hervy@spark-vision.com>
2021-02-06 17:27:07 +0100

    Updated w some react and css snippets.

--- sippets-objects.js
+++ sippets-objects.js
@@ -12,15 +12,15 @@ defaults({ a: 1 }, { b: 2 }, { b: 6 }, { a: 3 }); // { a: 1, b: 2 }
  * This snippet returns the first key that satisfies a given function.
  */
 const findKey = (obj, fn) =>
-  Object.keys(obj).find(key => fn(obj[key], key, obj));
+  Object.keys(obj).find((key) => fn(obj[key], key, obj));
 
 findKey(
   {
     barney: { age: 36, active: true },
     fred: { age: 40, active: false },
-    pebbles: { age: 1, active: true }
+    pebbles: { age: 1, active: true },
   },
-  o => o['active']
+  (o) => o['active']
 ); // 'barney'
 
 /**
@@ -28,15 +28,15 @@ findKey(
  * This snippet iterates on each property of an object and iterates a callback for each one respectively.
  */
 const forOwn = (obj, fn) =>
-  Object.keys(obj).forEach(key => fn(obj[key], key, obj));
-forOwn({ foo: 'bar', a: 1 }, v => console.log(v)); // 'bar', 1
+  Object.keys(obj).forEach((key) => fn(obj[key], key, obj));
+forOwn({ foo: 'bar', a: 1 }, (v) => console.log(v)); // 'bar', 1
 
 /**
  * 70. isObject
  * This snippet can be used to check whether a provided value is an object. It uses the Object constructor to create an object wrapper for the given value.
  * If it is already an object, then an object type that corresponds to the given value will be returned. Otherwise, a new object will be returned.
  */
-const isObject = obj => obj === Object(obj);
+const isObject = (obj) => obj === Object(obj);
 
 isObject([1, 2, 3, 4]); // true
 isObject([]); // true
@@ -49,18 +49,18 @@ isObject(true); // false
  * 71. isObjectLike
  * This snippet can be used to check if a value is not null and that its typeof is “object”.
  */
-const isObjectLike = val => val !== null && typeof val === 'object';
+const isObjectLike = (val) => val !== null && typeof val === 'object';
 
 isObjectLike({}); // true
 isObjectLike([1, 2, 3]); // true
-isObjectLike(x => x); // false
+isObjectLike((x) => x); // false
 isObjectLike(null); // false
 
 /**
  * 72. isPlainObject
  * This snippet checks whether a value is an object created by the Object constructor.
  */
-const isPlainObject = val =>
+const isPlainObject = (val) =>
   !!val && typeof val === 'object' && val.constructor === Object;
 
 isPlainObject({ a: 1 }); // true
@@ -72,7 +72,7 @@ isPlainObject(new Map()); // false
  */
 const matches = (obj, source) =>
   Object.keys(source).every(
-    key => obj.hasOwnProperty(key) && obj[key] === source[key]
+    (key) => obj.hasOwnProperty(key) && obj[key] === source[key]
   );
 
 matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true }); // true
@@ -82,7 +82,12 @@ matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true }); 
  * 104. shallowClone
  * This snippet can be used to create a shallow clone of an object.
  */
-const shallowClone = obj => Object.assign({}, obj);
+const shallowClone = (obj) => Object.assign({}, obj);
 
 const a = { x: true, y: 1 };
 const b = shallowClone(a); // a !== b
+
+/**
+ * Create an empty object without any inheritance
+ */
+const a = Object.create(null);
--- snippets-react.js
+++ snippets-react.js
@@ -16,3 +16,53 @@ import { ReactComponent as MyIcon } from "./icon.svg"
 // In render method.
 <MyIcon /> // or...
 <MyIcon className="someClassThatWillBeUsedInCSS" alt="icon" />
+
+
+/**
+ * Implement useState with useReducer.
+ */
+const [name, setName] = useReducer((_, value) => value, 'James');
+<input value={name} onChange={e => setName(e.target.value)} />
+
+
+/**
+ * Manually Re-render a Component.
+ */
+const [, rerender] = useState()
+rerender({})
+
+
+/**
+ * Pass a Function to setState
+ */
+const [count, setCount] = useState(0)
+setCount(c => c + 1)
+
+// Example
+const [count, setCount] = useState(0)
+useEffect(() => {
+  const id = setInterval(() => {
+    setCount(c => c + 1);
+  }, 1000);
+  return () => clearInterval(id);
+}, []);
+
+
+/**
+ * String Values as HTML Elements
+ */
+const Button = ({ Component = 'button', ...props }) => <Component {...props} />
+
+<Button>A Button</Button> // Renders a button element
+<Button Component="a">A Link</Button> // Renders an anchor element
+
+
+/**
+ * React Keys with Fragments
+ */
+pokemons.map(pokemon => (
+  <React.Fragment key={pokemon}>
+    <strong>Name: </strong>
+    <span>{pokemon}</span>
+  </React.Fragment>
+))
